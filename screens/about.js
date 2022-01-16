afc4a420881f5144a910e025c429824a3cc5a4c0
Owen Xue <owensit98@gmail.com>
2022-01-16 11:57:22 -0500

    added About Screen

---
+++ screens/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Text>This is the About Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
