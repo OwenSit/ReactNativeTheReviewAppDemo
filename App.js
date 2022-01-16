014c3f06787e81a197988a6376fcbab8d9795640
Owen Xue <owensit98@gmail.com>
2022-01-16 14:14:50 -0500

    loaded fonts to app

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import * as Font from "expo-font";
 import { StyleSheet, Text, View } from "react-native";
 import About from "./screens/about";
 import Home from "./screens/home";
 import ReviewDetails from "./screens/reviewDetails";
+import AppLoading from "expo-app-loading";
+
+const getFont = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
 
 export default function App() {
-  return (
-    <View>
-      <Home />
-      <About />
-      <ReviewDetails />
-    </View>
-  );
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  if (fontsLoaded) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFont}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
