e6bed2a3f02de19bf8862ede1fe0cab17527ce05
Owen Xue <owensit98@gmail.com>
2022-01-18 17:09:48 -0500

    passing data from home to details

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Text, View, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function ReviewDetails({ navigation }) {
+export default function ReviewDetails({ route, navigation }) {
+  const { item } = route.params;
   const pressHandler = () => {
     navigation.goBack();
   };
 
   return (
     <View style={globalStyles.container}>
-      <Text>This is the ReviewDetails Screen!</Text>
+      <Text>{item.title}</Text>
+      <Text>{item.body}</Text>
+      <Text>{item.rating}</Text>
       <Button title="back to home screen" onPress={pressHandler} />
     </View>
   );
